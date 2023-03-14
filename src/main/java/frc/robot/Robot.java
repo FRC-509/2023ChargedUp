b811da5c819f9d6e8c4622990211360f9038622a
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2023-03-14 12:29:06 -0400

    add arm pivot and extension to Thunderstorm

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -56,7 +56,7 @@ public class Robot extends TimedRobot {
 		// robot's periodic
 		// block in order for anything in the Command-based framework to work.
 		CommandScheduler.getInstance().run();
-		thunderstorm.update(this.robotContainer.swerveSubsystem.getModuleStates());
+		thunderstorm.update(this.robotContainer);
 	}
 
 	/** This function is called once each time the robot enters Disabled mode. */
--- src/main/java/frc/robot/util/telemetry/Thunderstorm.java
+++ src/main/java/frc/robot/util/telemetry/Thunderstorm.java
@@ -3,6 +3,7 @@ package frc.robot.util.telemetry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.RobotContainer;
 
 public class Thunderstorm {
 	private final NetworkTable table;
@@ -11,10 +12,13 @@ public class Thunderstorm {
 		table = NetworkTableInstance.getDefault().getTable("Thunderstorm");
 	}
 
-	public void update(SwerveModuleState[] states) {
+	public void update(RobotContainer container) {
+		SwerveModuleState[] states = container.swerveSubsystem.getModuleStates();
 		for (int number = 0; number < states.length; number++) {
 			table.getEntry("Module" + number + "Velocity").setDouble(states[number].speedMetersPerSecond);
 			table.getEntry("Module" + number + "Angle").setDouble(states[number].angle.getDegrees());
 		}
+		table.getEntry("ArmPivot").setDouble(container.armSubsystem.getPivotDegrees());
+		table.getEntry("ArmExtension").setDouble(container.armSubsystem.getExtensionPosition());
 	}
 }
